984e9b49a4b9d72123c49997c53bc4d3282d1060
jordan-liubenov <jordan.liubenov62@gmail.com>
2022-10-10 16:19:41 +0300

    implemented search and find functionality for the target port

--- port-killer.py
+++ port-killer.py
@@ -1,27 +1,45 @@
-# Script intended to make the termination of in-use ports easy
-from operator import indexOf
+# Script intended to make the finding and termination of leftover in-use ports easy
 import os
 
 def formatPortResults(found_ports):
+  formatted_ports = []
   found_ports = found_ports.split('\n')
   for i in found_ports:
-    current_index = found_ports.index(i)
-    print(i)
     if i != '':
       formatted_cell = i.split()
-      found_ports[current_index] = formatted_cell
-  return found_ports
+      formatted_ports.append(formatted_cell)
+  return formatted_ports
+
+def checkEstablished(port):
+  isEstablished = False
+  if port[3] == 'ESTABLISHED':
+    isEstablished = True
+  return isEstablished
 
 def searchAndDestroy(port):
   output = os.popen('netstat -ano | findstr :' + port).read()
   output = formatPortResults(output)
-  # print(output)
+
+  for i in output:
+    host = i[1].split(':')
+    current_port = host[1]
+    pid = i[4]
+    
+    if not checkEstablished(i) or current_port != port:
+      continue
+    
+    # TODO execute termination command:
+    # taskkill /PID 39304 /F
+
+
+
 
 def receiveInput():
-  target_port = input('Enter desired port to kill:\n')
-  
+  target_port = input('Enter desired port to kill:\n')  
+  target_port = target_port.strip()
+
   if int(target_port) <= 1023:
-  # check to make sure the port that was input is not a reserved port (0 - 1023)
+  # check to make sure the port that was input is not a reserved port
     decision = input('Sorry, that port is reserved, (retry) or (exit)?\n')
     decision = decision.strip()
     if decision == 'exit':
